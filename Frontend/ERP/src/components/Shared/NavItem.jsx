2ebc9bec8c695fabda9eb281b169220dae9bc2a6
M.Khalid.K.Agha <mkhalid.k.agha@gmail.com>
2023-03-14 20:41:15 +0200

    SideBar Active Item

--- Frontend/ERP/src/components/Shared/NavItem.jsx
+++ Frontend/ERP/src/components/Shared/NavItem.jsx
@@ -11,8 +11,11 @@ const NavItem = ({ Icon, title, onClick, linkTo, setActiveNavItem, activeNavItem
                     rounded-xl overflow-hidden cursor-pointer transition duration-200 ease-in-out
                      hover:bg-orange hover:text-light ${activeNavItem === title ? "text-[#f0f0f0] bg-orange" : ""}`}
                 >
-                    {Icon && <Icon className='icon text-xl ' />}
-                    <h2 className="text-base font-semibold font-montserrat ">
+                    {Icon && <Icon className={`text-lg
+                     ${activeNavItem === title ? "text-[#f0f0f0]" : ""}`} />}
+                    <h2 className={`
+                     ${activeNavItem === title ? "text-[#f0f0f0]" : ""}
+                    text-base font-semibold font-montserrat `}>
                         {title ? title : null}
                     </h2>
                 </div>
