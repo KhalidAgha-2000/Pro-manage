c4a25a6221f70a5c3c8222f06ef038d5dae171e2
M.Khalid.K.Agha <mkhalid.k.agha@gmail.com>
2023-03-15 21:13:01 +0200

    LoggrdIn Admin

--- Frontend/ERP/src/components/Admins.jsx
+++ Frontend/ERP/src/components/Admins.jsx
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Buttons from './Shared/Buttons'
-import { AiOutlineUserDelete, AiFillEdit, AiOutlineInfoCircle } from "react-icons/ai";
+import { AiOutlineUserDelete, AiFillPushpin, AiFillEdit, AiOutlineInfoCircle } from "react-icons/ai";
 import axiosInstance from '../constants/axios';
 import { Context } from './Context/Context';
 import { MdAdminPanelSettings } from 'react-icons/md';
+import { BsPinFill } from 'react-icons/bs';
 import { motion } from "framer-motion";
 import Loading from './Shared/Loading';
 const Admins = () => {
@@ -42,36 +43,49 @@ const Admins = () => {
     return (
         <div className='w-full h-[75vh] flex flex-wrap justify-center p-3 gap-x-4 gap-y-4 overflow-auto scrollbar-thin scrollbar-thumb-orange scrollbar-track-dark '>
             {
-                allAdminsData.map((admin) => (
-                    <motion.div
-                        initial={{ opacity: 0, y: 100 }}
-                        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-                        transition={{ duration: 0.5 }}
-                        key={admin._id}
-                        className="relative w-[32%] h-[200px] 
-                                   max-w-sm py-5 rounded-lg shadow-md font-montserrat shadow-orange bg-dark ">
-                        <div className="flex flex-col items-center ">
+                allAdminsData
+                    .sort((loggedIn) => {
+                        if (loggedIn._id === localStorage.getItem('id')) {
+                            return -1; // admin1 should come first
+                        } else {
+                            return 0; // keep the same order
+                        }
+                    })
+                    .map((admin) => (
+                        <motion.div
+                            initial={{ opacity: 0, y: 100 }}
+                            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+                            transition={{ duration: 0.5 }}
+                            key={admin._id}
+                            className={`relative w-[32%] h-[200px] 
+                                   max-w-sm py-5 rounded-lg shadow-md font-montserrat shadow-orange bg-dark 
+                                   `}>
+                            <div className="flex flex-col items-center ">
 
-                            {
-                                admin.image &&
-                                <img
-                                    src={admin.image} alt="admin-image"
-                                    className="w-24 h-24 mb-3 object-center aspect-square border-4 border-orange shadow-lg shadow-light rounded-full"
-                                /> ||
-                                <MdAdminPanelSettings className='text-orange w-24 h-24 ' />
-                            }
+                                {
+                                    admin.image &&
+                                    <img
+                                        src={admin.image} alt="admin-image"
+                                        className="w-24 h-24 mb-3 object-center aspect-square border-4 border-orange shadow-lg shadow-light rounded-full"
+                                    /> ||
+                                    <MdAdminPanelSettings className='text-orange w-24 h-24 ' />
+                                }
 
-                            <h5 className="mb-1 text-2xl font-bold  text-light">{admin.username}</h5>
-                            <span className="text-sm text-light">{admin.email}</span>
-                        </div>
-                        {/* Tools */}
-                        <div className='h-full w-4 absolute top-0 pr-4 right-2 flex flex-col items-start  justify-evenly  text-light'>
-                            <AiFillEdit className='hover:scale-150  transition duration-200 ease-in-out' size={20} color='#e04e17' cursor={'pointer'} />
-                            <AiOutlineUserDelete className='hover:scale-150  transition duration-200 ease-in-out' size={20} color='#e04e17' cursor={'pointer'} />
-                            <AiOutlineInfoCircle className='hover:scale-150  transition duration-200 ease-in-out' size={20} color='#e04e17' cursor={'pointer'} />
-                        </div>
-                    </motion.div>
-                ))}
+                                <h5 className="mb-1 text-2xl font-bold  text-light">{admin.username}</h5>
+                                <span className="text-sm text-light">{admin.email}</span>
+                            </div>
+                            {/* Tools */}
+                            <div className='h-full w-4 absolute top-0 pr-4 right-3 flex flex-col items-start  justify-evenly  text-light'>
+                                <AiFillEdit className='hover:scale-150  transition duration-200 ease-in-out' size={20} color='#e04e17' cursor={'pointer'} />
+                                <AiOutlineInfoCircle className='hover:scale-150  transition duration-200 ease-in-out' size={20} color='#e04e17' cursor={'pointer'} />
+                                <AiOutlineUserDelete className={`hover:scale-150  transition duration-200 ease-in-out ${admin._id === localStorage.getItem('id') && "hidden"}`} size={20} color='#e04e17' cursor={'pointer'} />
+                            </div>
+                            {/* Pinned */}
+
+                            <AiFillPushpin size={20} color="#e04e17"
+                                className={`absolute -rotate-90 top-1 left-1 ${admin._id !== localStorage.getItem('id') && "hidden"}`} />
+                        </motion.div>
+                    ))}
 
         </div>
     )
--- Frontend/ERP/src/components/Shared/Loading.jsx
+++ Frontend/ERP/src/components/Shared/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
     return (
         <>
             {loading &&
-                <div className='m-auto w-[80%] h-[70%] opacity-80 fixed z-[9999] bg-slate-200 flex flex-col items-center justify-center'>
+                <div className='m-auto w-[80%] h-[80%] opacity-80 fixed z-[9999] bg-slate-200 flex flex-col items-center justify-center'>
                     <div className="loader-wrapper w-12 h-12">
                         <div className="loader animate-spin w-full h-full border-4  border-t-orange border-b-dark rounded-full">
                             <div className="loader-inner duration-200 border-b-[red] border-t-pink-300"></div>
