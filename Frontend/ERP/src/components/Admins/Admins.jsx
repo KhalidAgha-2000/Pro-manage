91ac00f1ec46f2389ebc7f82e2ac0ba75c5e82b2
M.Khalid.K.Agha <mkhalid.k.agha@gmail.com>
2023-04-03 18:02:22 +0300

    Admins Style

--- Frontend/ERP/src/components/Admins/Admins.jsx
+++ Frontend/ERP/src/components/Admins/Admins.jsx
@@ -117,17 +117,16 @@ const Admins = (props) => {
                                 whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
                                 transition={{ duration: 0.5 }}
                                 key={admin._id}
-                                className={`relative w-[32%] h-[200px] 
-                                   max-w-sm py-5 rounded-lg shadow-md font-montserrat shadow-orange bg-dark `
+                                className={`relative w-[32%] h-[200px] max-w-sm py-5 rounded-lg shadow-lg font-montserrat overflow-hidden shadow-orange bg-dark `
                                 }
                             >
-                                <div className="flex flex-col items-center ">
+                                <div className="flex flex-col items-center">
 
                                     {
                                         admin.image &&
                                         <img
                                             src={admin.image} alt="admin-image"
-                                            className="w-24 h-24 mb-3 object-center aspect-square border-4 border-orange shadow-lg shadow-light rounded-full"
+                                            className="w-24 h-24 mb-3 object-cover object-center border-orange shadow-lg shadow-light rounded-full"
                                         /> ||
                                         <MdAdminPanelSettings className='text-orange w-24 h-24 ' />
                                     }
@@ -138,14 +137,14 @@ const Admins = (props) => {
                                 {/* Tools */}
                                 <div className='h-full w-4 absolute top-0 pr-4 right-3 flex flex-col items-start  justify-evenly  text-light'>
                                     <Link to={"/dashboard/admins/admin/" + admin._id}>
-                                        <AiFillEdit className='hover:scale-150  transition duration-200 ease-in-out' size={20} color='#e04e17' cursor={'pointer'} />
+                                        <AiFillEdit className='hover:scale-150  transition duration-200 ease-in-out' size={20} color='#f0f0f0' cursor={'pointer'} />
                                     </Link>
                                     <AiOutlineUserDelete className={`hover:scale-150  transition duration-200 ease-in-out ${admin._id === props.idInToken && "hidden"}`}
-                                        size={20} color='#e04e17' cursor={'pointer'} onClick={() => { setPrepareToRemove(admin._id) }}
+                                        size={20} color='#f0f0f0' cursor={'pointer'} onClick={() => { setPrepareToRemove(admin._id) }}
                                     />
                                 </div>
                                 {/* Pinned */}
-                                <AiFillPushpin size={20} color="#e04e17"
+                                <AiFillPushpin size={20} color="#f0f0f0"
                                     className={`absolute -rotate-90 top-1 left-1 ${admin._id !== props.idInToken && "hidden"}`}
                                 />
                                 {/* Delete */}
@@ -153,12 +152,17 @@ const Admins = (props) => {
                                     <GiCheckMark size={40} cursor={'pointer'} color="#4bb543" onClick={() => { removeAdmin(admin._id) }} />
                                     <TbLetterX size={40} cursor={'pointer'} color="#ff3333" onClick={() => { setPrepareToRemove('') }} />
                                 </div>
+
+                                {/* ---- */}
+                                <span className='absolute  right-2 -bottom-2 w-4 h-4 object-cover bg-[#dfd6d3] bg-opacity-95 rounded-full'></span>
+                                <span className='absolute  right-2 bottom-4 w-1 h-1 object-cover bg-[#dfd6d3] bg-opacity-95 rounded-full'></span>
+
                             </motion.div>
                         ))
                 )
             }
             {/* Add Button */}
-            <Link className='fixed z-[9999] right-6 bottom-6' to={"/dashboard/add-admin/"}>
+            <Link className='fixed z-[9999] right-6 bottom-6' to={"/dashboard/admins/add-admin/"}>
                 <IconButtons Icon={MdAdd} />
             </Link>
 
