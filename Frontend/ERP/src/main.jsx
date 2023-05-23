e2f21b98e75268ebab0ea84fce2ada0d17d92589
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2023-05-23 19:58:24 +0300

    Projects : Context

--- Frontend/ERP/src/main.jsx
+++ Frontend/ERP/src/main.jsx
@@ -5,12 +5,15 @@ import './index.css';
 import { ContextBody } from './Context/Context';
 import { AdminProvider } from './Context/AdminContext';
 import { EmployeeProvider } from './Context/EmployeeeContext';
+import { ProjectProvider } from './Context/ProjectContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ContextBody>
     <AdminProvider>
       <EmployeeProvider>
-        <App />
+        <ProjectProvider>
+          <App />
+        </ProjectProvider>
       </EmployeeProvider>
     </AdminProvider>
   </ContextBody>
