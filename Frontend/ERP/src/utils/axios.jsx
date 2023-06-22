6f5d4a534fdad78d44a00ad14dd2d992fff7e798
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2023-06-22 21:29:31 +0300

    Deploy F_002 CORS

--- Frontend/ERP/src/utils/axios.jsx
+++ Frontend/ERP/src/utils/axios.jsx
@@ -4,12 +4,12 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: 'https://pro-manager-server.vercel.app/api/v1',
     timeout: 40000,
-    // headers: {
-    //     'Content-Type': 'application/json',
-    //     "Access-Control-Allow-Origin": "*",
-    //     "Access-Control-Allow-Headers": "Origin, X- Requested - With, Content- Type, Accept"
-    // },
-
+    headers: {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X- Requested - With, Content- Type, Accept"
+    },
+    withCredentials: true,
 });
 
 export default axiosInstance;
