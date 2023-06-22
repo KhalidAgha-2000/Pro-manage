3bfe535daf2ec6d9f1a602b5500f15379579112d
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2023-06-22 21:03:39 +0300

    Deploy B_002 CORS

--- Backend/app.js
+++ Backend/app.js
@@ -29,7 +29,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(5000 || process.env.PORT)
 
