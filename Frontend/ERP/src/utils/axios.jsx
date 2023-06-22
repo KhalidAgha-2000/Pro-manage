3e014ddddc51d5dbd2a48ca3e8896be9fcb5fc43
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2023-06-22 21:10:07 +0300

    Deploy F_002 CORS

--- Frontend/ERP/src/utils/axios.jsx
+++ Frontend/ERP/src/utils/axios.jsx
@@ -6,9 +6,10 @@ const axiosInstance = axios.create({
     timeout: 40000,
     headers: {
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Origin, X- Requested - With, Content- Type, Accept"
+        // "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Headers": "Origin, X- Requested - With, Content- Type, Accept"
     },
+
 });
 
 export default axiosInstance;
