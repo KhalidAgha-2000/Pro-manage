883b779f8003a131ba7756ba6df4cb5bac7a7ea6
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2023-06-25 11:28:41 +0300

    Deploy B_009 CORS

--- Backend/app.js
+++ Backend/app.js
@@ -29,7 +29,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(cors());
 app.use(cors({
     origin: [
         'https://pro-manage-v1.vercel.app',
